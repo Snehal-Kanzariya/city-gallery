e27edca051f0c662412aac5c94db115618691081
Snehal-Kanzariya <snehal.kanzariya02@gmail.com>
2023-03-28 09:21:53 +0530

    updated image grid layout

--- src/pages/components/Header/Header.js
+++ src/pages/components/Header/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <header className="h-20 flex items-center shadow-2xl px-5">
-        <h1 className="text-3xl font-bold">Logo.</h1>
+      <header className="h-20 flex items-center shadow-2xl px-5 mb-5">
+        <h1 className="text-3xl font-black">Logo.</h1>
       </header>
     </>
   );
--- src/pages/components/ImageGrid/ImageGrid.js
+++ src/pages/components/ImageGrid/ImageGrid.js
@@ -1,28 +1,41 @@
 import React from "react";
-// import city1 from "../../../images/city1.jpeg";
 import Image from "next/image";
+// import "./ImageGrid.css";
 import gridData from "../../../../public/data.json";
 
 const ImageGrid = () => {
   return (
-    <div className="grid grid-cols-4 gap-4 image-grid py-5">
-      {gridData.items.map((item, index) => {
-        return (
-          <div key={index}>
-            <Image
-              src={item.image}
-              alt="city1"
-              width={300}
-              height={300}
-              className="h-[200px]"
-            />
-            <div>
-              <h4 className="text-xl font-bold p-2.5">{item.title}</h4>
+    <>
+      <div className="grid grid-cols-1 px-3 pt-3">
+        <h3 className="text-3xl font-bold">Browse by Location</h3>
+      </div>
+      <div className="gallery-container flex flex-wrap justify-between p-5">
+        {gridData.items.map((item, index) => {
+          return (
+            <div
+              className="gallery-item mb-5 w-[30%] rounded-md bg-white overflow-hidden hover:-translate-y-[5px] "
+              key={index}
+            >
+              <Image
+                src={item.image}
+                alt="city"
+                width={300}
+                height={300}
+                className="w-full h-auto"
+              />
+              <div className="item-details p-2.5">
+                <h3 className="m-0 text-xl font-semibold text-gray-800">
+                  {item.title}
+                </h3>
+                <p className="mt-2.5 text-sm text-gray-800">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
